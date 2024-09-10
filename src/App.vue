d15e6cdb1b8acc8b111d1ec1316c9b402544193a
Jose Luis Villalba <joseluisvillalba11@gmail.com>
2024-09-10 11:03:53 -0300

    agregando lo trabajado en la clase 4 del portafolio web, creacion de la barra de navegacion

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup>
-const nombreApellido = 'José Luis Villalba';
-
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
   <Header>
-    <h1>{{ nombreApellido }}</h1>
-    <p>Bienvenido a Vue 3</p>
+    <!--Barra de Navegacion-->
+    <NavBar/>
   </Header>
-
+  <main>
+      <section id="educacion"><h2>Educación - Cursos</h2></section>
+      <section id="experiencia"><h2>Experiencia</h2></section>
+      <section id="proyectos"><h2>Proyectos</h2></section>
+      <section id="habilidades"><h2>Habilidades</h2></section>
+      <section id="intereses"><h2>Intereses</h2></section>
+  </main>
 </template>
 
 <style scoped>
