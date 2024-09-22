0edc35c4cadb6514745e5fd10fc33e92fcce5c84
Jose Luis Villalba <joseluisvillalba11@gmail.com>
2024-09-22 17:24:27 -0300

    agregando lo trabajado en la clase 6 del portafolio

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
 import DatosPersonales from './components/DatosPersonales.vue';
-
+import Educacion from './components/Educacion.vue';
 </script>
 
 <template>
@@ -12,7 +12,10 @@ import DatosPersonales from './components/DatosPersonales.vue';
     <DatosPersonales />
   </Header>
   <main>
-      <section id="educacion"><h2>Educación - Cursos</h2></section>
+      <!--el estilo CSS de Section se encuentra en base.css-->
+      <section id="educacion"><h2>Educación - Cursos</h2>
+      <Educacion/>
+      </section>
       <section id="experiencia"><h2>Experiencia</h2></section>
       <section id="proyectos"><h2>Proyectos</h2></section>
       <section id="habilidades"><h2>Habilidades</h2></section>
