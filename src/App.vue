454671a3461098434e43f01c558950f4e47f1889
Jose Luis Villalba <joseluisvillalba11@gmail.com>
2024-09-29 11:42:11 -0500

    agregando lo trabajado en el portafolio clase 7, sección Experiencia

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import NavBar from './components/NavBar.vue';
 import DatosPersonales from './components/DatosPersonales.vue';
 import Educacion from './components/Educacion.vue';
+import ExperienciaComponente from './components/ExperienciaComponente.vue';
 </script>
 
 <template>
@@ -16,7 +17,9 @@ import Educacion from './components/Educacion.vue';
       <section id="educacion"><h2>Educación - Cursos</h2>
       <Educacion/>
       </section>
-      <section id="experiencia"><h2>Experiencia</h2></section>
+      <section id="experiencia"><h2>Experiencia</h2>
+      <ExperienciaComponente />
+      </section>
       <section id="proyectos"><h2>Proyectos</h2></section>
       <section id="habilidades"><h2>Habilidades</h2></section>
       <section id="intereses"><h2>Intereses</h2></section>
